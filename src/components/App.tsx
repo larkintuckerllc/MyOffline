a1e30ed4d2beec1ca10203681ffd67b3ce61ec62
John Tucker <jtucker@appirio.com>
2019-10-16 13:38:50 -0400

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { Text, View } from 'react-native';
+
+const App: FC = () => (
+  <View>
+    <Text>Hello App</Text>
+  </View>
+);
+
+export default App;
