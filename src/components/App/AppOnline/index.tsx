455ab1cd1df867c082c4d2cce96c2a5a2249d0e5
John Tucker <jtucker@appirio.com>
2019-10-17 16:26:41 -0400

    Toggle online

---
+++ src/components/App/AppOnline/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useCallback } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { Dispatch } from 'redux';
+import { ActionType } from '../../../store/ducks';
+import { getOnline, setOnline } from '../../../store/ducks/online';
+import styles from './styles';
+
+const AppOnline: FC = () => {
+  const dispatch = useDispatch<Dispatch<ActionType>>();
+  const online = useSelector(getOnline);
+  const handlePress = useCallback(() => {
+    dispatch(setOnline(!online));
+  }, [online]);
+
+  return (
+    <View style={styles.root}>
+      <Text>
+        Online:
+        {online ? 'True' : 'False'}
+      </Text>
+      <TouchableOpacity onPress={handlePress}>
+        <Text>Toggle</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default AppOnline;
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,17 +1,38 @@
 import { ApolloProvider } from '@apollo/react-hooks';
-import React, { FC } from 'react';
-import { Provider } from 'react-redux';
+import React, { FC, useState } from 'react';
+import { Text } from 'react-native';
+import { Provider, useSelector } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import client from '../../graphql/client';
+import store, { persistor } from '../../store';
+import { getOnline } from '../../store/ducks/online';
 import AppBooks from './AppBooks';
 import AppBooksCreate from './AppBooksCreate';
-import store from '../../store';
+import AppOnline from './AppOnline';
 
-const App: FC = () => (
-  <Provider store={store}>
+const AppUsingRedux: FC = () => {
+  const online = useSelector(getOnline);
+  const [cachePersisted, setCachePersisted] = useState(false);
+
+  /*
+  if (!cachePersisted) {
+    return <Text>Loading Apollo Client Persistence or Online</Text>;
+  }
+  */
+  return (
     <ApolloProvider client={client}>
+      <AppOnline />
       <AppBooksCreate />
       <AppBooks />
     </ApolloProvider>
+  );
+};
+
+const App: FC = () => (
+  <Provider store={store}>
+    <PersistGate loading={<Text>Loading Redux Persistence</Text>} persistor={persistor}>
+      <AppUsingRedux />
+    </PersistGate>
   </Provider>
 );
 
